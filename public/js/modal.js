25c4c78cd26573b02401e76a7584ab7b82a0e079
Francis. S.H. Kinosita <hideyoshi515@gmail.com>
2024-09-07 18:48:51 +0900

    modal tag add

--- public/js/modal.js
+++ public/js/modal.js
@@ -8,18 +8,17 @@ function openModal(shopid) {
   <div style="display:flex;justify-content: center;">
   <img src="img/loading.gif" style="padding: 10vh 0; width: 50%;">
   </div>`;
-  
-  // モーダルを表示し、スクロール無効化
-  modal.style.display = "block"; 
-  document.body.style.overflow = "hidden"; // スクロール無効
 
+  // モーダルを表示し、スクロール無効化
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden"; // スクロール無効
   const url = `${apiUrl}&id=${shopid}`;
   fetch(proxyUrl + encodeURIComponent(url))
     .then((response) => response.text())
     .then((data) => parseModalXML(data))
     .catch((error) => console.error("Error:", error))
     .finally(() => {
-    }); 
+    });
 }
 
 function parseModalXML(xmlString) {
@@ -35,30 +34,25 @@ function handleModalData(shop) {
   const logoImage = shop.getElementsByTagName("l")[0].textContent;
   const address = shop.getElementsByTagName("address")[0].textContent;
   const open = shop.getElementsByTagName("open")[0].textContent;
-  const private_room = shop
-    .getElementsByTagName("private_room")[0]
-    .textContent.includes("あり")
-    ? "個室あり"
-    : "個室なし";
-  const cardCan = shop
-    .getElementsByTagName("card")[0]
-    .textContent.includes("不可")
-    ? "カード払い利用不可"
-    : "カード払い利用可";
-    addItemToList(`${shopid}§${name}§${access}§${logoImage}§${address}§${open}`);
-    saveListToLocalStorage("shopHistory");
+  const genre = shop.getElementsByTagName("genre")[0].getElementsByTagName("name")[0].textContent;
+  const private_room = shop.getElementsByTagName("private_room")[0].textContent.includes("あり") ? "個室あり" : "個室なし";
+  const cardCan = shop.getElementsByTagName("card")[0].textContent.includes("不可") ? "カード払い利用不可" : "カード払い利用可";
+  const parking = shop.getElementsByTagName("parking")[0].textContent.includes("なし") ? "駐車不可" : "駐車可";
+  const non_smoking = shop.getElementsByTagName("non_smoking")[0].textContent;
+  addItemToList(`${shopid}§${name}§${access}§${logoImage}§${address}§${open}`);
+  saveListToLocalStorage("shopHistory");
   // モーダルの本文更新
   modalBody.innerHTML = `
   <div>
     <div>
-        <img class="shop-image" src="${logoImage}" alt="${name}">
-        <p class="shop-name" style="white-space: wrap">${name}</p>
-        <p class="shop-access" style="white-space: wrap"> ${address}</p>
-        <p class="shop-access" style="white-space: wrap"> ${access}</p>
-        <p class="shop-time" style="white-space: wrap"> ${open}</p>
-    </div>
-    <div>
-        <p class="shop-tag"> #${private_room} #${cardCan}</p>
+      <img class="shop-image" src="${logoImage}" alt="${name}">
+      <p class="shop-name" style="white-space: wrap">${name}</p>
+      <p class="shop-access" style="white-space: wrap"> ${address}</p>
+      <p class="shop-access" style="white-space: wrap"> ${access}</p>
+      <p class="shop-time" style="white-space: wrap"> ${open}</p>
+      </div>
+      <div>
+      <p class="shop-tag"> #${private_room} #${cardCan} #${parking} #${non_smoking} #${genre}</p>
     </div>
   </div>
 `;
