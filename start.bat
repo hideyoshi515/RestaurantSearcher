04f6f773bfa7bea3244bc9dde0b6962a980ec2d2
Francis. S.H. Kinosita <30834275+hideyoshi515@users.noreply.github.com>
2024-09-09 10:45:52 +0900

    Delete start.bat

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: node_modulesフォルダが存在するか確認
-if not exist "node_modules" (
-    echo node_modulesフォルダがありません。npm installを実行します...
-    npm install
-) else (
-    echo node_modulesフォルダが存在します。サーバーを起動します...
-    npm start
-)
