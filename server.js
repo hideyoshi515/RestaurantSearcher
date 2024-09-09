00c251b6607062eaaa2e2a9b7863c44c488ea7d5
hideyoshi515 <hideyoshi515@gmail.com>
2024-09-09 14:52:45 +0900

    Revert "Update server.js"
    This reverts commit 07677ebce2e1841adb3d4d429036002866193f6b.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const fs = require("fs");
-const https = require("https");
-const cors = require("cors");
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
 
 // SSLキーと証明書の設定
-const sslKey = fs.readFileSync("./localhost.key", "utf8");
-const sslCert = fs.readFileSync("./localhost.pem", "utf8");
+const sslKey = fs.readFileSync('./localhost.key', 'utf8');
+const sslCert = fs.readFileSync('./localhost.pem', 'utf8');
 
 // アプリケーションの初期化
 const app = express();
@@ -14,16 +14,14 @@ const app = express();
 app.use(cors());
 
 // 静的ファイルを提供
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // HTTPサーバーをポート3000で起動
 app.listen(3000, () => {
-  console.log("HTTPサーバーはポート3000で実行中");
+  console.log('HTTPサーバーはポート3000で実行中');
 });
 
 // HTTPSサーバーをポート3001で起動
-https
-  .createServer({ key: sslKey, cert: sslCert, passphrase: "necohost515" }, app)
-  .listen(3001, () => {
-    console.log("HTTPSサーバーはポート3001で実行中");
-  });
+https.createServer({ key: sslKey, cert: sslCert }, app).listen(3001, () => {
+  console.log('HTTPSサーバーはポート3001で実行中');
+});
